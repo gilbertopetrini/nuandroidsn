14a3f97fba0bdefe4e72cea1060fd3980365de48
petrinidias <petrini4u@gmail.com>
2025-06-22 15:13:58 -0300

    coisaboa67

--- src/Pix.jsx
+++ src/Pix.jsx
@@ -19,9 +19,9 @@ import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion';
 
 const pageVariants = {
-  initial: { opacity: 0, x: 100 },
+  initial: { opacity: 1, x: 100 },
   animate: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: -100 },
+  exit: { opacity: 1, x: -100 },
 };
 
 function Pix() {
